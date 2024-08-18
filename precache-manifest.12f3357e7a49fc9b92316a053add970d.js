7968827441a708c4339c5c2199e42c5e6b18d186
ankitgoyal0301 <goyalankit3129@gmail.com>
2024-08-18 18:50:21 +0530

    Deploy React app

---
+++ precache-manifest.12f3357e7a49fc9b92316a053add970d.js
@@ -0,0 +1,370 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f8ee77f14f71a4a61df67cae44e4f93",
+    "url": "/Ankit-Portfolio/index.html"
+  },
+  {
+    "revision": "872370fa9006bad165e6",
+    "url": "/Ankit-Portfolio/static/css/main.c3d5beb4.chunk.css"
+  },
+  {
+    "revision": "c5190a9d5bc517667cdd",
+    "url": "/Ankit-Portfolio/static/js/2.77aab95b.chunk.js"
+  },
+  {
+    "revision": "872370fa9006bad165e6",
+    "url": "/Ankit-Portfolio/static/js/main.f3e30049.chunk.js"
+  },
+  {
+    "revision": "b0994655cf8cc95bb23e",
+    "url": "/Ankit-Portfolio/static/js/runtime-main.5a3bafba.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/Ankit-Portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/Ankit-Portfolio/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/Ankit-Portfolio/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/Ankit-Portfolio/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/Ankit-Portfolio/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/Ankit-Portfolio/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/Ankit-Portfolio/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Ankit-Portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "dcebd89cd13f8dae720f00c0104502ff",
+    "url": "/Ankit-Portfolio/static/media/Pec_coa_logo.dcebd89c.png"
+  },
+  {
+    "revision": "ef2c5f95b918675bb83a7757a738c946",
+    "url": "/Ankit-Portfolio/static/media/acm_logo.ef2c5f95.png"
+  },
+  {
+    "revision": "16559172ca1d4d552390b48f19e102ba",
+    "url": "/Ankit-Portfolio/static/media/address_image.16559172.svg"
+  },
+  {
+    "revision": "adf73005c329357a4417e6421b9bbbf0",
+    "url": "/Ankit-Portfolio/static/media/animated_ankit.adf73005.jpg"
+  },
+  {
+    "revision": "eaf02c2d0158b6b4b8c2a0a0cf830a04",
+    "url": "/Ankit-Portfolio/static/media/animated_ashutosh.eaf02c2d.png"
+  },
+  {
+    "revision": "dcd6dee2bd7458a4a576dc131506fd83",
+    "url": "/Ankit-Portfolio/static/media/animated_logo.dcd6dee2.svg"
+  },
+  {
+    "revision": "87f087d64e154ed8c66d70b2934456e7",
+    "url": "/Ankit-Portfolio/static/media/apple_logo.87f087d6.gif"
+  },
+  {
+    "revision": "b9eb2303803d6b8f1d3ea36221001ac5",
+    "url": "/Ankit-Portfolio/static/media/apple_logo.b9eb2303.jpeg"
+  },
+  {
+    "revision": "c38be63d4efaba9ff64be3ad917c3dd2",
+    "url": "/Ankit-Portfolio/static/media/apple_logo.c38be63d.png"
+  },
+  {
+    "revision": "a7e580cb3bd89c20500fce34a3b63e56",
+    "url": "/Ankit-Portfolio/static/media/aws_logo.a7e580cb.png"
+  },
+  {
+    "revision": "2e4367d2658f5d55c014d8394ee0d8e5",
+    "url": "/Ankit-Portfolio/static/media/blockchain_logo.2e4367d2.png"
+  },
+  {
+    "revision": "058eca4cbdc03a1dad9a6bb943ac7831",
+    "url": "/Ankit-Portfolio/static/media/blogs_image.058eca4c.svg"
+  },
+  {
+    "revision": "283176fd176465475f1a91652f5254c5",
+    "url": "/Ankit-Portfolio/static/media/chd-baptist-logo.283176fd.png"
+  },
+  {
+    "revision": "cacbc42b661f39344af81c631d8d36a9",
+    "url": "/Ankit-Portfolio/static/media/cloud_infrastructure.cacbc42b.svg"
+  },
+  {
+    "revision": "fee92c18b1cfbd1faa704c51047f113d",
+    "url": "/Ankit-Portfolio/static/media/codeInLogo.fee92c18.png"
+  },
+  {
+    "revision": "a5a05a88fb79513192b28c57306b927a",
+    "url": "/Ankit-Portfolio/static/media/codechef_logo.a5a05a88.png"
+  },
+  {
+    "revision": "cafd0bcdaa5d285d74b4c031ecca5141",
+    "url": "/Ankit-Portfolio/static/media/codechef_logo.cafd0bcd.jpeg"
+  },
+  {
+    "revision": "5f915405511f649dad433a6db9adbff4",
+    "url": "/Ankit-Portfolio/static/media/contactMail.5f915405.png"
+  },
+  {
+    "revision": "ebf35d7f33ebbd4a4c6cff7e66e21fdb",
+    "url": "/Ankit-Portfolio/static/media/data_science.ebf35d7f.svg"
+  },
+  {
+    "revision": "75a7107b7739a46950f66de29779eb52",
+    "url": "/Ankit-Portfolio/static/media/deeplearning_ai_logo.75a7107b.png"
+  },
+  {
+    "revision": "3e23ca14e7a7c8a7b63d255466749000",
+    "url": "/Ankit-Portfolio/static/media/developerActivity.3e23ca14.svg"
+  },
+  {
+    "revision": "63f707f224c8a842ac928c36e3be879a",
+    "url": "/Ankit-Portfolio/static/media/dsc_logo.63f707f2.png"
+  },
+  {
+    "revision": "3bab7a9dc4324f1b2364d25a47fcfcf0",
+    "url": "/Ankit-Portfolio/static/media/education.3bab7a9d.svg"
+  },
+  {
+    "revision": "86b356aa4636232f3e200c65d2a8b6b4",
+    "url": "/Ankit-Portfolio/static/media/ethereum_logo.86b356aa.png"
+  },
+  {
+    "revision": "a00ede3f70759480e81f14f203edaa51",
+    "url": "/Ankit-Portfolio/static/media/experience.a00ede3f.svg"
+  },
+  {
+    "revision": "0017cc6de3f72f67753efa37bb796fbd",
+    "url": "/Ankit-Portfolio/static/media/fa-brands-400.0017cc6d.svg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/Ankit-Portfolio/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/Ankit-Portfolio/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/Ankit-Portfolio/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/Ankit-Portfolio/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/Ankit-Portfolio/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/Ankit-Portfolio/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/Ankit-Portfolio/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "e647ddbb6d95cb01deac07ffafb2a085",
+    "url": "/Ankit-Portfolio/static/media/fa-regular-400.e647ddbb.svg"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/Ankit-Portfolio/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/Ankit-Portfolio/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/Ankit-Portfolio/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "424c59fb1df79fcc3d0a41909c0192d2",
+    "url": "/Ankit-Portfolio/static/media/fa-solid-900.424c59fb.svg"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/Ankit-Portfolio/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/Ankit-Portfolio/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "2936ce95f550e3feef7230ea543a4b60",
+    "url": "/Ankit-Portfolio/static/media/feelingProud.2936ce95.svg"
+  },
+  {
+    "revision": "fce6080726685cabdb4f75a0d8018325",
+    "url": "/Ankit-Portfolio/static/media/fullstack.fce60807.svg"
+  },
+  {
+    "revision": "0ee7e8c9444cdfefc10f6d86e18b9b5b",
+    "url": "/Ankit-Portfolio/static/media/gcp_logo.0ee7e8c9.png"
+  },
+  {
+    "revision": "6eeb2e810d0fd9f3ca2dcd72de228e68",
+    "url": "/Ankit-Portfolio/static/media/github_logo.6eeb2e81.png"
+  },
+  {
+    "revision": "0c8a4c114ad00f7854e91550379b9b91",
+    "url": "/Ankit-Portfolio/static/media/googleAssistant.0c8a4c11.jpg"
+  },
+  {
+    "revision": "a6addc3fb6605480614b992ede8cd434",
+    "url": "/Ankit-Portfolio/static/media/googleAssistant.a6addc3f.svg"
+  },
+  {
+    "revision": "b577ad44e4cc4850849a22f9aae0ff49",
+    "url": "/Ankit-Portfolio/static/media/google_logo.b577ad44.webp"
+  },
+  {
+    "revision": "5385baa9137a172608a87e48ae74dab1",
+    "url": "/Ankit-Portfolio/static/media/gs_logo.5385baa9.png"
+  },
+  {
+    "revision": "d3fccbe1db76b96f4f320d9c0f7da30c",
+    "url": "/Ankit-Portfolio/static/media/ibm_logo.d3fccbe1.png"
+  },
+  {
+    "revision": "1daf1e54f507272bf67ff0632cd9813a",
+    "url": "/Ankit-Portfolio/static/media/icpc_logo.1daf1e54.png"
+  },
+  {
+    "revision": "571a8584e69ecfbe326c8b8385fbbf54",
+    "url": "/Ankit-Portfolio/static/media/iiitk_logo.571a8584.png"
+  },
+  {
+    "revision": "29152cf756a1facead5393c32e373371",
+    "url": "/Ankit-Portfolio/static/media/intel_logo.29152cf7.jpg"
+  },
+  {
+    "revision": "60ed0bd3f8b99ac1d29e74575f804aaa",
+    "url": "/Ankit-Portfolio/static/media/iu_logo.60ed0bd3.png"
+  },
+  {
+    "revision": "5b4a6449747ec61a6d9aa874f2a9ceba",
+    "url": "/Ankit-Portfolio/static/media/jsFramework.5b4a6449.svg"
+  },
+  {
+    "revision": "2ad6bd55153d1ec87a2c0ad58e9ac3a6",
+    "url": "/Ankit-Portfolio/static/media/learning_how_to_learn_logo.2ad6bd55.png"
+  },
+  {
+    "revision": "c597fbded5de799dfb9abc3fc4f1c26f",
+    "url": "/Ankit-Portfolio/static/media/legato_logo.c597fbde.png"
+  },
+  {
+    "revision": "854363c93cd72cb927c6979b1d5d938d",
+    "url": "/Ankit-Portfolio/static/media/linkedin_logo.854363c9.png"
+  },
+  {
+    "revision": "83ac24fb968a474c6a63f7a4e9a8f62c",
+    "url": "/Ankit-Portfolio/static/media/manOnTable.83ac24fb.svg"
+  },
+  {
+    "revision": "069938a84edd6ebf383c318e9feb5d4c",
+    "url": "/Ankit-Portfolio/static/media/muffito_logo.069938a8.png"
+  },
+  {
+    "revision": "3d5e9b31f844e5781395b183b04cea80",
+    "url": "/Ankit-Portfolio/static/media/name-logo.3d5e9b31.png"
+  },
+  {
+    "revision": "889156bb739e2c53c27dd3496fc4f1a1",
+    "url": "/Ankit-Portfolio/static/media/name-logo.889156bb.svg"
+  },
+  {
+    "revision": "a0a5d40fbd5d9266cb1c6d5b4b8c9558",
+    "url": "/Ankit-Portfolio/static/media/name-logo2.a0a5d40f.png"
+  },
+  {
+    "revision": "7d665bff33bdd202f614c27b695d08b7",
+    "url": "/Ankit-Portfolio/static/media/name-logo3.7d665bff.png"
+  },
+  {
+    "revision": "4d26228f524880450eecdcff22b812d5",
+    "url": "/Ankit-Portfolio/static/media/name-logo4.4d26228f.png"
+  },
+  {
+    "revision": "68a3fc791151de48bce5a0c596a2df2f",
+    "url": "/Ankit-Portfolio/static/media/normal_ankit.68a3fc79.png"
+  },
+  {
+    "revision": "95a12a2b8c3149cce2175679755c4b4d",
+    "url": "/Ankit-Portfolio/static/media/nptel_logo.95a12a2b.png"
+  },
+  {
+    "revision": "926ae5a053c4f9661da29673b29ae03b",
+    "url": "/Ankit-Portfolio/static/media/pec_logo.926ae5a0.png"
+  },
+  {
+    "revision": "414dfbd98516b39255a3e22ed90b3f7d",
+    "url": "/Ankit-Portfolio/static/media/portfolio.414dfbd9.gif"
+  },
+  {
+    "revision": "5faf790badc18fa9030f94836ece1df1",
+    "url": "/Ankit-Portfolio/static/media/programmer.5faf790b.svg"
+  },
+  {
+    "revision": "b8ba948796d7ab532673c5ed2f315e74",
+    "url": "/Ankit-Portfolio/static/media/projects_image.b8ba9487.svg"
+  },
+  {
+    "revision": "71408db71465afd742233e4a96f06526",
+    "url": "/Ankit-Portfolio/static/media/pwa.71408db7.png"
+  },
+  {
+    "revision": "d5edca398c182c2b24d226131eed737f",
+    "url": "/Ankit-Portfolio/static/media/sggs_chd_logo.d5edca39.png"
+  },
+  {
+    "revision": "7031f549be5d10874e0aa1c2c42eb185",
+    "url": "/Ankit-Portfolio/static/media/smart_contracts_logo.7031f549.png"
+  },
+  {
+    "revision": "2c497738be3831492aeb6bdc43f19154",
+    "url": "/Ankit-Portfolio/static/media/stanford_logo.2c497738.png"
+  },
+  {
+    "revision": "6ac91b161c2c9934a44f72d75d18142d",
+    "url": "/Ankit-Portfolio/static/media/talksCardBack.6ac91b16.svg"
+  },
+  {
+    "revision": "3fa5424232370e3d049c9555d9c440a4",
+    "url": "/Ankit-Portfolio/static/media/talksCardBackAlt.3fa54242.svg"
+  },
+  {
+    "revision": "dda17a1f7f51df2f97e4d68e369c7115",
+    "url": "/Ankit-Portfolio/static/media/techfest_logo.dda17a1f.png"
+  },
+  {
+    "revision": "0993e3246bf5146154391a0afef79b03",
+    "url": "/Ankit-Portfolio/static/media/tiktok_logo.0993e324.png"
+  },
+  {
+    "revision": "d63630893eb8cb64af65f395d393c481",
+    "url": "/Ankit-Portfolio/static/media/ui_ux_design.d6363089.svg"
+  }
+]);
